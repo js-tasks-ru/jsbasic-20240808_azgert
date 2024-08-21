8970f3fe878aba18d8ae9eb771b3a94928361047
iSentir <azgert@yandex.ru>
2024-08-21 12:47:25 +0300

    Правка 3.1/3.4

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,14 +1,4 @@
-let vasya = { name: 'Вася', age: 25 };
-let petya = { name: 'Петя', age: 30 };
-let masha = { name: 'Маша', age: 28 };
-
-let users = [vasya, petya, masha];
-
 function namify(users) {
-  const arrUser = [];
 
-  users.forEach(el => arrUser.push(el.name));
-
-  return arrUser;
+  return users.map((el)=> el.name);
 }
-
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,19 +1,8 @@
 function showSalary(users, age) {
-  if (Array.isArray(users)) {
-    return users
-      .flat()
-      .filter((el) => el.age <= age)
-      .map((el) => {
-        return `${el.name}, ${el.balance}`;
-      })
-      .join('\n');
-  } else if (typeof users === 'object') {
-    for ( const key in users ) {
-      if ( users.age <= age ) {
-        return `${users.name}, ${users.balance} `;
-      }
-    }
-  } else {
-    return false;
-  }
+  return users
+    .filter((el) => el.age <= age)
+    .map((el) => {
+      return `${el.name}, ${el.balance}`;
+    })
+    .join('\n');
 }
