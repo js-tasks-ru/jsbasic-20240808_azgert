a7eaa1b023b661e4f3201e41cbcc1aec43b7e85f
iSentir <azgert@yandex.ru>
2024-09-09 11:42:28 +0300

    task 5.3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
-  // ваш код...
+  const slideL = document.querySelector(".carousel__arrow_left");
+  const slideR = document.querySelector(".carousel__arrow_right");
+  const slideAll = document.querySelector('.carousel__inner');
+  const sLength = document.querySelectorAll('.carousel__slide').length - 1;
+  const offSet = slideAll.offsetWidth;
+  slideL.style.display = 'none';
+
+  let i = 0;
+
+  slideL.addEventListener('click', () => {
+    update(--i);
+    slideAll.style.transform = `translateX(-${i * offSet}px)`;
+  });
+
+  slideR.addEventListener('click', () => {
+    update(++i);
+    slideAll.style.transform = `translateX(-${i * offSet}px)`;
+  });
+
+  function update(i) {
+    slideL.style.display = i === 0 ? 'none' : '';
+    slideR.style.display = i === sLength ? 'none' : '';
+  }
 }
